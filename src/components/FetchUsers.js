929b6eb076e34670d932fcbe80de36677856da12
kazuki827 <xm.kazuki827@gmail.com>
2022-05-04 08:29:49 +0900

    suspense

---
+++ src/components/FetchUsers.js
@@ -0,0 +1,21 @@
+import { useQueryUsers } from '../hooks/useQueryUsers'
+
+export const FetchUsers = () => {
+  /* suspenseを使わない場合 コンポーネントレベルで状態のハンドリングが必要
+  const { status, data } = useQueryUsers()
+  if (status === 'loading') return <p>Loading</p>
+  if (status === 'error') return <p>Error</p>
+  */
+  const { data } = useQueryUsers()
+  
+  return (
+    <div className="my-3 text-center">
+      <p className="my-3 font-bold">User List</p>
+      {data?.map((user) => (
+        <p className="my-3 text-sm" key={user.id}>
+          {user.username}
+        </p>
+      ))}
+    </div>
+  )
+}
---
+++ src/components/SuspenseDemo.js
@@ -0,0 +1,46 @@
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ExclamationCircleIcon } from '@heroicons/react/solid'
+
+import { Layout } from './Layout'
+import { Spinner } from './Spinner'
+import { FetchUsers } from './FetchUsers'
+import { FetchTasks } from './FetchTasks'
+import { FetchComments } from './FetchComments'
+
+export const SuspenseDemo = () => {
+  return (
+    <Layout>
+      <p className="mb-3 text-xl font-bold text-blue-500">Suspense Demo</p>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchUsers />
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchTasks />
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchComments />
+        </Suspense>
+      </ErrorBoundary>
+    </Layout>
+  )
+}
